e339ebaf70e4af73bd8189b0f78f322fb799f16c
sktmdgus1212 <sktmdgus1212@naver.com>
2024-06-11 09:50:54 +0900

    feat: initial setting

---
+++ src/clustering/components/scatter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function scatter() {
+  return <div>zz</div>;
+}
---
+++ src/main/pages/main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Scatter from "../../clustering/components/scatter";
+
+export default function main() {
+  return <Scatter />;
+}
