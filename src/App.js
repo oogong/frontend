b2f5127f6c67c14ee657e8fdfc681e26e786a86d
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-11 15:40:01 +0900

    ⚡ update: main 파일에 Navbar 추가 후 App에서 import 하는 것으로 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import NavbarHeader from "./components/Navbar/Navbar";
+import Main from "./main";
 
 function App() {
   return (
     <div className="App">
-      <NavbarHeader />
+      <Main />
     </div>
   );
 }
