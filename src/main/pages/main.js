8d9f780e752a7f586bfa65731336de326365ceda
InGyu Na <43513001+sktmdgus1212@users.noreply.github.com>
2024-06-11 16:33:57 +0900

    feat: add scatter chart via nivo (#9)
    * feat: initial setting
    
    * feat: create scatter chart connected server

---
+++ src/main/pages/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Scatter from "../../clustering/components/scatter";
+import { Navbar } from "react-bootstrap/Navbar";
+
+export default function main() {
+  return (
+    <>
+      <Navbar />
+      <Scatter />
+    </>
+  );
+}
