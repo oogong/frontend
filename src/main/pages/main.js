ef2e056405e5f443b3661f482e9a50e8f064277f
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-12 16:25:15 +0900

    ✨ feat: bootstrap 사용해서 layout 분류

--- src/main/pages/main.js
+++ src/main/pages/main.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Scatter from "../../clustering/components/scatter";
-import { Navbar } from "react-bootstrap/Navbar";
+import NavbarHeader from "../../components/Navbar/Navbar";
+import CustomGraph from "../../custom-graph/pages/main";
+import CustomGraphCompare from "../../custom-graph/pages/compare";
+import { Row, Col, Container } from "react-bootstrap";
 
 export default function main() {
   return (
     <>
-      <Navbar />
-      <Scatter />
+      <NavbarHeader />
+      <Container fluid>
+        <Row>
+          <Col>
+            <CustomGraph />
+          </Col>
+          <Col>
+            <CustomGraphCompare />
+          </Col>
+        </Row>
+        {/* <Scatter /> */}
+      </Container>
     </>
   );
 }
