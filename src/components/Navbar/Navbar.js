f415f6ba42760d91fea776dc428448a8fe38599a
InGyu Na <43513001+sktmdgus1212@users.noreply.github.com>
2024-06-12 16:33:50 +0900

    feat: reload page from search api (#11)
    * feat: load page from searching
    
    * fix: add exception

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,31 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+// import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
+import { useState } from "react";
+import { getSearchResult } from "./apis/NavbarApi";
 
 function NavbarHeader() {
+  const [keyword, setKeyword] = useState("");
+  // const navigate = useNavigate();
+
+  const handleKeyword = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const searchKeyword = async (event) => {
+    event.preventDefault();
+    const searchResult = await getSearchResult(keyword);
+    if (searchResult.code === 404) {
+      alert("해당 종목을 찾을 수 없습니다.");
+      return;
+    }
+    console.log(searchResult.code);
+    // navigate(`/detail?keyword=${searchResult.code}`); // 추후 router로 페이지 이동 작성
+  };
+
   return (
     <Navbar expand="lg" className="nav-body">
       <Container fluid>
@@ -25,14 +46,22 @@ function NavbarHeader() {
             <Nav.Link href="#action1">순위 비교</Nav.Link>
             <Nav.Link href="#action2">서비스 소개</Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex" onSubmit={searchKeyword}>
             <Form.Control
               type="search"
               placeholder="종목명으로 검색해보세요."
               className="me-2"
               aria-label="Search"
+              value={keyword}
+              onChange={handleKeyword}
             />
-            <img src="/search-icon.svg" alt="Search" width="20" />
+            <Button
+              variant="outline-light"
+              type="submit"
+              className="search-button"
+            >
+              <img src="/search-icon.svg" alt="Search" width="20" />
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
--- src/main/pages/main.js
+++ src/main/pages/main.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Scatter from "../../clustering/components/scatter";
-import { Navbar } from "react-bootstrap/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 
 export default function main() {
   return (
