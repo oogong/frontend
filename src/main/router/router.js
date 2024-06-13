2ab7f1aced1df518a2601e8a28fa76ce53401968
sktmdgus1212 <sktmdgus1212@naver.com>
2024-06-13 16:39:56 +0900

    fix: fix nginx error

--- src/main/router/router.js
+++ src/main/router/router.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Main from "../pages/main";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 const router = createBrowserRouter([
+  { path: "/", element: <Navigate to="/compare" replace /> },
   { path: "/compare", element: <Main />, index: true },
   { path: "/{code}", element: <></>, index: true },
 ]);
