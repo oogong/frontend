e3a97de5190e40b49f0930e8a9d4f5d794d52877
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-14 13:46:46 +0900

    🐛 fix: 비교 순위 컴포넌트로 변경

--- src/main/pages/main.js
+++ src/main/pages/main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavbarHeader from "../../navbar/components/navbar";
 import CustomGraph from "../../custom-graph/pages/main";
+import CustomGraphCompare from "../../custom-graph/pages/compare";
 import "./styles/style.css";
 import Lineup from "../../weightedgraph/lineup";
 import Lineup2 from "../../weightedgraph/lineup2";
 import { Row, Col, Container } from "react-bootstrap";
 import { WeightProvider } from "../../weightedgraph/weightcontext";
+import { WeightProviderCompare } from "../../weightedgraph/weightcontext2";
 
 export default function main() {
   return (
@@ -20,10 +22,10 @@ export default function main() {
             </WeightProvider>
           </Col>
           <Col xs={6}>
-            <WeightProvider>
-              <CustomGraph />
-              <Lineup2></Lineup2>
-            </WeightProvider>
+            <WeightProviderCompare>
+              <CustomGraphCompare />
+              <Lineup2 />
+            </WeightProviderCompare>
           </Col>
         </Row>
       </Container>
