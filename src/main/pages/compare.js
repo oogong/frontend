5de5e34fb03be09c38e930917d9d1e895c67edfc
kyeongseo90 <rudtj9099@gmail.com>
2024-06-20 14:18:52 +0900

    ✨feat: main, compare 분할

--- src/main/pages/compare.js
+++ src/main/pages/compare.js
@@ -5,25 +5,28 @@ import "./styles/style.css";
 import Lineup from "../../weightedgraph/lineup";
 import { Row, Col, Container } from "react-bootstrap";
 import { WeightProvider } from "../../weightedgraph/weightcontext";
-import Scatter from "../../clustering/components/scatter";
 
-export default function main() {
+export default function Compare() {
   const initialSliderValues = [30, 25, 15, 20, 10];
+  const initialSliderValues2 = [20, 20, 20, 20, 20];
 
   return (
     <div className="main-page">
       <NavbarHeader />
       <Container fluid>
-        <Row className="align-items-start">
-          <WeightProvider initialSliderValues={initialSliderValues}>
-            <Col xs={6}>
+        <Row>
+          <Col xs={6}>
+            <WeightProvider initialSliderValues={initialSliderValues}>
               <CustomGraph uniqueId="compare1" title="순위" />
               <Lineup />
-            </Col>
-            <Col xs={6}>
-              <Scatter width={"100%"} height={"300px"}/>
-            </Col>
-          </WeightProvider>
+            </WeightProvider>
+          </Col>
+          <Col xs={6}>
+            <WeightProvider initialSliderValues={initialSliderValues2}>
+              <CustomGraph uniqueId="compare2" title="비교순위" />
+              <Lineup />
+            </WeightProvider>
+          </Col>
         </Row>
       </Container>
     </div>
--- src/main/router/router.js
+++ src/main/router/router.js
@@ -2,10 +2,11 @@ import React from "react";
 import Main from "../pages/main";
 import Detail from "../pages/detail";
 import { createBrowserRouter, Navigate } from "react-router-dom";
+import Compare from "../pages/compare";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Navigate to="/compare" replace /> },
-  { path: "/compare", element: <Main />, index: true },
+  { path: "/", element: <Main />, index: true},
+  { path: "/compare", element: <Compare />, index: true },
   { path: "/:code", element: <Detail />, index: true },
 ]);
 
