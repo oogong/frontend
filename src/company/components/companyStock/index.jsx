e5407be66fef76210e9589dd30c5cc406a4c6188
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-26 23:52:49 +0900

    ✨ feat: 소켓 함수 불러와서 상세페이지 좌측 상단의 종목 정보에 표시

--- src/company/components/companyStock/index.jsx
+++ src/company/components/companyStock/index.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import './../styles/companyStock.css'
+import React, { useEffect, useState } from "react";
+import "./../styles/companyStock.css";
+import { connectSocket, joinRoom } from "../../service/stockPrice";
+import {
+  disconnectSocket,
+  requestCurrentPrice,
+} from "../../service/stockPrice";
 
 export default function CompanyStock(props) {
+  const [stockCompare, setStockCompare] = useState(0);
+  const [compare, setCompare] = useState(0);
+  const [stockPrice, setStockPrice] = useState(props.price);
+  const [color, setColor] = useState("red");
+
+  const handlePrice = (loadedPrice) => {
+    setStockPrice(loadedPrice);
+  };
+
+  const handleCompare = (loadedCompare) => {
+    setStockCompare(loadedCompare);
+  };
+
+  useEffect(() => {
+    console.log("code: ", props.code);
+    connectSocket();
+
+    joinRoom(props.code, handlePrice, handleCompare)
+      .then((price) => {
+        console.log("Price loaded:", price);
+      })
+      .catch((error) => {
+        console.error("Error joining price room:", error);
+      });
+
+    requestCurrentPrice(props.code, handlePrice, handleCompare);
+
+    return () => {
+      disconnectSocket();
+    };
+  }, [props.code]);
   return (
-    <div style={{display:"flex", flexDirection:"column", paddingRight:"10px", marginRight:'10px', borderRight:"2px solid #c1c1c1"}}>
-      <p className='name'>{props.name}</p>
-      <p className='code'>{props.code}</p>
-      <p className='price'>{props.price}</p>
+    <div className="company-stock">
+      <p className="name">{props.name}</p>
+      <p className="code">{props.code}</p>
+      <p className="price" style={{ color: color }}>
+        {stockPrice}
+      </p>
+      <div className="up-and-down">
+        <img
+          src={color === "#FF0000" ? "up-icon.svg" : "down-icon.svg"}
+          alt="up-and-down"
+          style={{ width: "15px", height: "15px" }}
+        />
+        <p className="compare" style={{ color: color }}>
+          {compare}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
