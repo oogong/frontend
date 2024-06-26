b36a7a0d335166373aae66e725050713360273fa
kyeongseo90 <rudtj9099@gmail.com>
2024-06-27 08:59:33 +0900

    💡update: 지표 순서 변경

--- src/company/index.jsx
+++ src/company/index.jsx
@@ -6,17 +6,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Matrix from './components/metrics';
 import './components/styles/company.css';
 
-export default function Company({ratios}) {
-  const indicatorName = ['수익성', '활동성', '안정성', '성장성'];
-  const rateColor = ['#8884d8', '#82ca9d', '#ffc658', '#ff7300'];
+export default function Company({ ratios }) {
+  const indicatorName = ['수익성', '안정성', '성장성', '활동성'];
+  const rateColor = ['#ff7300', '#ffc658', '#82ca9d', '#8884d8'];
 
-  // TODO: 글꼴을 적용합니다.
   return (
     <>
-    {
-      ratios &&
-        <Container fluid style={{padding:"30px 30px 0px 30px"}}>
-          <div style={{display:"flex", flexDirection:"row"}}>
+      {
+        ratios &&
+        <Container fluid style={{ padding: "30px 30px 0px 30px" }}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
             <CompanyStock code={ratios.code} name={ratios.name} price={ratios.price} />
             <OgongRate rate={ratios.ogong_rate} />
           </div>
